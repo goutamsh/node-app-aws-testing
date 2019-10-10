766165d18db9f7486a16cd9dd43aa40ca03bd8a5
Goutam Shepur <goutam.s.h@gmail.com>
2019-10-10 22:37:32 +0100

    commit

--- scripts/create-ec2-instance.js
+++ scripts/create-ec2-instance.js
@@ -104,7 +104,7 @@ function createInstance (sgName, keyName) {
       SecurityGroups :[
           sgName
       ],
-      UserData: 'Y3VybCAtLXNpbGVudCAtLWxvY2F0aW9uIGh0dHBzOi8vcnBtLm5vZGVzb3VyY2UuY29tL3NldHVwXzgueCB8IHN1ZG8gYmFzaCAtCnN1ZG8geXVtIGluc3RhbGwgLXkgbm9kZWpzCnN1ZG8geXVtIGluc3RhbGwgLXkgZ2l0CmdpdCBjbG9uZSBodHRwczovL2dpdGh1Yi5jb20vZ291dGFtc2gvbm9kZS1hcHAtYXdzLXRlc3RpbmcuZ2l0CmNkIG5vZGUtYXBwLWF3cy10ZXN0aW5nCm5wbSBpCm5vZGUgaW5kZXguanM'
+      UserData: 'Y3VybCAtLXNpbGVudCAtLWxvY2F0aW9uIGh0dHBzOi8vcnBtLm5vZGVzb3VyY2UuY29tL3NldHVwXzgueCB8IHN1ZG8gYmFzaCAtDQpzdWRvIHl1bSBpbnN0YWxsIC15IG5vZGVqcw0Kc3VkbyB5dW0gaW5zdGFsbCAteSBnaXQNCmdpdCBjbG9uZSBodHRwczovL2dpdGh1Yi5jb20vZ291dGFtc2gvbm9kZS1hcHAtYXdzLXRlc3RpbmcuZ2l0DQpjZCBub2RlLWFwcC1hd3MtdGVzdGluZw0KbnBtIGkNCm5vZGUgaW5kZXguanM='
     
     }
 
